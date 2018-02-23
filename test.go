80287962f7bc5463f4a6291f0da6567221cca3e9
Jabrail Alizadeh <jabrail.alizadeh@VTBAZE.local>
2018-02-23 17:10:25 +0400

    types

--- test.go
+++ test.go
@@ -3,5 +3,43 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Printf("Test\n")
+
+    //Default value
+    var num0 int
+
+    //Initialized with value
+    var num1 int = 1
+
+    //Without strict showing type
+    var num2 = 20
+
+    fmt.Println(num0, num1, num2)
+    
+    //Short initialization of variable
+    num3 := 30
+
+    num3 += 1
+    fmt.Println("+=", num3)
+
+    num3++
+    fmt.Println("++", num3)
+
+    //camelCase accepted standart
+    userIndex := 10
+
+    //underscore is not acceptable
+    user_index := 10
+
+    fmt.Println(userIndex, user_index)
+
+    //Several var init
+    var weight, height int = 10, 20
+
+    //Assign values
+    weight, height = 11, 21
+
+    //Shortcut
+    weight, age := 12, 22
+
+    fmt.Println(weight, height, age)
 }
